b82ba9acfd32732d3f56aef3b318ea1c74d54aec
viethahong <abc@gmail.com>
2014-07-13 22:26:02 +0700

    upload

---
+++ asg01/Beer.java
@@ -0,0 +1,12 @@
+package asg01;
+
+public class Beer {
+	public static void main(String [] args){
+		System.out.println("HaHongViet\t11020391 \tK56C!\tviethahong\tviethh_56@vnu.edu.vn");
+		for(int i=9;i>=1;i--){
+			System.out.println(i+" bottles of beer on the wall, "+i+"bottles of beer.\n	Take one down, pass it around,\n");
+		}
+		System.out.println("No more bottles of beer on the wall.");
+	}
+
+}
